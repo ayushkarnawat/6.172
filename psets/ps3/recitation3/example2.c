c94b05af80215fa05827fda23eeafcdab9b46a60
ayushkarnawat <ayush.karnawat97@gmail.com>
2020-09-11 10:45:41 -0400

    Vectorize using ternary expression

--- psets/ps3/recitation3/example2.c
+++ psets/ps3/recitation3/example2.c
@@ -7,13 +7,11 @@
 #define SIZE (1L << 16)
 
 void test(uint8_t * restrict a, uint8_t * restrict b) {
-  uint64_t i;
+  a = __builtin_assume_aligned(a, 16);
+  b = __builtin_assume_aligned(b, 16);
 
-  uint8_t * x = __builtin_assume_aligned(a, 16);
-  uint8_t * y = __builtin_assume_aligned(b, 16);
-
-  for (i = 0; i < SIZE; i++) {
-    /* max() */
-    if (y[i] > x[i]) x[i] = y[i];
+  // Find the (row-wise) max of both arrays
+  for (uint64_t i = 0; i < SIZE; i++) {
+    a[i] = (b[i] > a[i]) ? b[i] : a[i];
   }
 }
